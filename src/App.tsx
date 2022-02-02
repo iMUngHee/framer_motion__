a3140b43266be42b21752b4eef01d24f055f0348
Hee <unghee66@gmail.com>
2022-02-02 20:25:37 +0900

    END

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
 import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Gird = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
+const Box = styled(motion.div)`
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: white;
-  border-radius: 10px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 function App() {
+  const [id, setId] = useState<null | string>(null);
   return (
     <Wrapper>
-      <Box />
+      <Gird>
+        {["1", "2", "3", "4"].map((n) => (
+          <Box onClick={() => setId(n)} key={n} layoutId={n} />
+        ))}
+      </Gird>
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            initial={{ background: "rgba(0, 0, 0, 0)" }}
+            animate={{ background: "rgba(0, 0, 0, 0.5)" }}
+            exit={{ background: "rgba(0, 0, 0, 0)" }}
+            onClick={() => setId(null)}
+          >
+            <Box style={{ width: 400, height: 200 }} layoutId={id} />
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
